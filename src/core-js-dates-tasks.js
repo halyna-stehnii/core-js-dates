843056856a6dd3672ecdca1f0e1b093ca18dd620
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-03-25 00:20:35 +0100

    getWorkSchedule solved

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -299,8 +299,29 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const [startDay, startMonth, startYear] = period.start.split('-').map(Number);
+  const [endDay, endMonth, endYear] = period.end.split('-').map(Number);
+  const startDate = new Date(startYear, startMonth - 1, startDay);
+  const endDate = new Date(endYear, endMonth - 1, endDay);
+  const workSchedule = [];
+  const currentDate = new Date(startDate);
+  let cyclePosition = 0;
+
+  while (currentDate <= endDate) {
+    if (cyclePosition < countWorkDays) {
+      const formattedDate = `${String(currentDate.getDate()).padStart(2, '0')}-${String(
+        currentDate.getMonth() + 1
+      ).padStart(2, '0')}-${currentDate.getFullYear()}`;
+      workSchedule.push(formattedDate);
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1);
+
+    cyclePosition = (cyclePosition + 1) % (countWorkDays + countOffDays);
+  }
+
+  return workSchedule;
 }
 
 /**
